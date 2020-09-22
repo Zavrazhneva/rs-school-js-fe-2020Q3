3190769d567dc3c20a3e93414e50fc7ad1fcc46c
Anastasiya Zavrazhneva <zavrazhneva.a@gmail.com>
2020-09-22 17:47:31 +0300

    feat: add clear function and output to html

--- calculator/script.js
+++ calculator/script.js
@@ -1,64 +1,86 @@
 const buttons = document.querySelectorAll('[data-number]');
 const operations = document.querySelectorAll('[data-operation]');
 const equals = document.querySelector('[data-equals]');
+const previousHtml = document.querySelector('.previous-operand');
+const currentHtml = document.querySelector('.current-operand');
+const clearAll = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
 
 class Calculator {
     constructor() {
         this.previousOperand = '';
         this.currentOperand = '';
-        this.readyResult = false;
-        this.result = undefined;
-        this.operator = ''
+        this.operator = '';
     }
 
     appendNumber(number) {
-        if(this.previousOperand !== '') this.readyResult = true;
-        this.currentOperand = this.currentOperand + number.toString();
+        this.currentOperand += number.toString();
+        this.appendInnerHtml(this.previousOperand, this.currentOperand)
+    }
+
+    setPreviousValue() {
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
     }
 
     appendOperator(operator) {
         this.operator = operator;
-        this.previousOperand = this.currentOperand;
-        this.currentOperand = '';
+        this.setPreviousValue();
+        this.appendInnerHtml(this.previousOperand + operator, this.currentOperand)
     }
+
+    clear() {
+        this.previousOperand = '';
+        this.currentOperand = '';
+        this.operator = '';
+        this.appendInnerHtml(this.previousOperand, this.currentOperand)
+    }
+
+    removeNumber() {
+        this.currentOperand.slice(0, -1)
+    }
+
     calculate() {
         let current = parseFloat(this.currentOperand);
         let previous = parseFloat(this.previousOperand);
         switch (this.operator) {
             case '+' :
-                this.result =  previous + current;
+                this.result = previous + current;
                 break;
-
             case '-' :
-                this.result =  previous - current;
+                this.result = previous - current;
                 break;
-
             case '*' :
-                this.result =  previous * current;
+                this.result = previous * current;
                 break;
-
             case '÷' :
-                this.result =  previous / current;
+                this.result = previous / current;
                 break;
         }
         console.log(this.result)
     }
-    equally() {
-        if (this.readyResult) {
-            this.calculate()
-        }
+
+    equalHandler() {
+        this.calculate();
+        this.clear();
+        this.appendInnerHtml(this.previousOperand, this.result)
     }
 
+    appendInnerHtml(previous , current) {
+        previousHtml.innerText = previous;
+        currentHtml.innerText = current;
+    }
 }
+let calculator = new Calculator(previousHtml, currentHtml);
 
-let calculator = new Calculator();
-
-buttons.forEach( button => {
+buttons.forEach(button => {
     button.addEventListener('click', () => calculator.appendNumber(button.innerText))
 })
 
-operations.forEach( button => {
+operations.forEach(button => {
     button.addEventListener('click', () => calculator.appendOperator(button.innerText))
 })
 
-equals.addEventListener('click', () => calculator.equally());
+equals.addEventListener('click', () => calculator.equalHandler());
+clearAll.addEventListener('click', () => calculator.clear());
+deleteButton.addEventListener('click', () => calculator.removeNumber());
