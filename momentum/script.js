59b2b655819a876d9a61d70a2657f6b6c99d4f5a
Anastasiya Zavrazhneva <zavrazhneva.a@gmail.com>
2020-10-24 12:26:29 +0300

    fix: refactor focus function

--- momentum/script.js
+++ momentum/script.js
@@ -8,20 +8,37 @@ const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
 const city = document.querySelector('.city');
+const figureChange = document.querySelector('.figure__change');
+const errorText = document.querySelector('.error-text');
+const weatherBlock = document.querySelector('.weather__block');
+
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
+getWeather()
 
 
 async function getWeather() {
-    let cityLocalStorage = localStorage.getItem('city');
-    if(cityLocalStorage === null) cityLocalStorage = 'Moscow';
-    city.innerHTML = cityLocalStorage;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityLocalStorage}&lang=ru&appid=6ed93d000e9fdd858d58b138c07b9c90&units=metric`;
-    const res = await fetch(url);
-    const data = await res.json();
-    weatherIcon.className = 'weather-icon owf';
+        let cityLocalStorage = localStorage.getItem('city');
+        if(cityLocalStorage === null) cityLocalStorage = 'Moscow';
+        city.innerHTML = cityLocalStorage;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityLocalStorage}&lang=ru&appid=6ed93d000e9fdd858d58b138c07b9c90&units=metric`;
+        const res = await fetch(url);
+        const data = await res.json();
 
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${Math.floor(data.main.temp)}°C`;
-    weatherDescription.textContent = data.weather[0].description;
+        if(data.cod >= 400) {
+            city.innerHTML = cityLocalStorage;
+            errorText.innerHTML = 'Город не найден';
+            weatherBlock.style.display = 'none';
+        } else {
+            weatherBlock.style.display = 'block';
+            errorText.innerHTML = '';
+            weatherIcon.className = 'weather-icon owf';
+            weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+            temperature.textContent = `${Math.floor(data.main.temp)}°C`;
+            weatherDescription.textContent = data.weather[0].description;
+        }
+
 }
 function setCity(event) {
     localStorage.setItem('city', event.target.innerText);
@@ -30,14 +47,13 @@ function setCity(event) {
         city.blur();
     }
 }
-document.addEventListener('DOMContentLoaded', getWeather);
-city.addEventListener('keypress', setCity);
-getWeather()
+
 
 btn.addEventListener('click', getImage);
 name.addEventListener('keypress',setName);
 name.addEventListener('blur',setName);
 focus.addEventListener('keypress',setFocus);
+focus.addEventListener('click',setFocus);
 focus.addEventListener('blur',setFocus);
 
 const showAmPm = true;
@@ -57,7 +73,7 @@ folders.forEach(folderName => {
         randomImagesArr.push(`assets/images/${folderName}/${randomNumbers[i]}`)
     }
 })
-//console.log(randomImagesArr);
+
 function randomInteger(min, max) {
     let rand = min - 0.5 + Math.random() * (max - min + 1);
     return Math.round(rand);
@@ -73,18 +89,20 @@ function getRandomNumbers() {
             numbersImage.push(images[randomNumber]);
         }
     }
-    console.log(numbers);
-    console.log(numbersImage);
     return numbersImage
 }
 
 fetch('https://type.fit/api/quotes').then(res => res.json()).then(list => {
-
-    let randomNumberQuote = randomInteger(1, list.length);
-
-    let randomQuote = list[randomNumberQuote]
-    cite.innerHTML = randomQuote.author;
-    blockquote.innerHTML = randomQuote.text;
+    function randomQuote() {
+        let randomNumberQuote = randomInteger(1, list.length);
+        let randomQuote = list[randomNumberQuote];
+        cite.innerHTML = randomQuote.author;
+        blockquote.innerHTML = randomQuote.text;
+    }
+    randomQuote()
+    figureChange.addEventListener('click', () => {
+        randomQuote()
+    })
 })
 
 
@@ -97,8 +115,8 @@ function showTime() {
         month = today.getMonth(),
         dayWeek = today.getDay()
 
-    const amOrPm = hour >= 12 ? 'pm': 'am ';
-    time.innerHTML = `${addZero(hour)}:${addZero(minute)}:${addZero(second)} ${showAmPm ? amOrPm : ''}`;
+
+    time.innerHTML = `${addZero(hour)}:${addZero(minute)}:${addZero(second)}`;
     date.innerHTML = `${daysInWeek[dayWeek]} ${day} ${monthText[month]}`;
     setTimeout(showTime,1000);
 }
@@ -109,8 +127,8 @@ function addZero(num) {
 
 
 function setBackgroundGreet() {
-    let today = new Date(),
-        hour = today.getHours();
+    let today = new Date();
+    let hour = today.getHours();
     const millsToNextHour = (60 - today.getMinutes())* 60 * 1000;
 
     setTimeout(() => {
@@ -118,17 +136,14 @@ function setBackgroundGreet() {
         setBackgroundGreet();
     }, millsToNextHour);
 
-    if(hour > 6 && hour < 12) {
-        //morning
-
+    if(hour >= 6 && hour < 12) {
         greeting.innerHTML = 'Good morning'
     }
-    if(hour > 12 && hour < 18) {
+    if(hour >= 12 && hour < 18) {
         //day
-
         greeting.innerHTML = 'Good day'
     }
-    if(hour > 18 && hour < 24) {
+    if(hour >= 18 && hour < 24) {
         //evening
 
         greeting.innerHTML = 'Good evening'
@@ -148,8 +163,8 @@ function getName() {
 }
 
 function getFocus() {
-    if(localStorage.getItem('focus') === null) {
-        focus.innerHTML = '[enter name]'
+    if(localStorage.getItem('focus') === null || localStorage.getItem('focus') === 'enter name') {
+        focus.innerHTML = 'enter name'
     } else {
         focus.innerHTML = localStorage.getItem('focus')
     }
@@ -167,14 +182,33 @@ function setName(event) {
 }
 
 function setFocus(event) {
+    if(event.type ==='click') {
+        focus.innerHTML = '';
+        return
+    }
+    if(event.type ==='blur') {
+        focus.innerHTML = event.target.innerText;
+        localStorage.setItem('focus', event.target.innerText);
+        if(event.target.innerText === '') {
+            event.target.innerText = 'enter name'
+        }
+        return
+    }
     if(event.type === 'keypress' ) {
         if(event.code === 'Enter' || event.code === 'NumpadEnter') {
+            if(event.target.innerText === '') {
+                event.target.innerText = 'enter name'
+            }
             localStorage.setItem('focus', event.target.innerText);
             focus.blur()
+            focus.innerHTML = event.target.innerText;
+            getFocus()
         }
     } else {
-        localStorage.setItem('focus', event.target.innerText)
+        localStorage.setItem('focus', event.target.innerText);
     }
+
+
 }
 let currentImage = (new Date()).getHours();
 
